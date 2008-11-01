819e006746929fe20fe2bc283602058641aad0b3
alexk <alexk>
2008-11-01 17:44:03 +0000

    [#328919] add user pictures to my relationships page 6.x

--- user_relationships_ui/templates/user_relationships.tpl.php
+++ user_relationships_ui/templates/user_relationships.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: user_relationships.tpl.php,v 1.1.2.4 2008-04-21 00:38:19 sprsquish Exp $
+// $Id: user_relationships.tpl.php,v 1.1.2.5 2008-11-01 17:44:03 alexk Exp $
 
 if ($relationships) {
   foreach ($relationships as $relationship) {
@@ -8,12 +8,16 @@ if ($relationships) {
     $this_user_str  = $account->uid == $relationship->requestee_id ? 'requester' : 'requestee';
     $this_user      = $relationship->{$this_user_str};
 
-    $rows[] = array(
+    $row = array(
       theme('username', $this_user),
       $relationship->name . ($relationship->is_oneway ? ($this_user_str == 'requester' ? t(' (You to Them)') : t(' (Them to You)')) : NULL),
       $relationship->extra_for_display,
       $edit_access ? theme('user_relationships_remove_link', $account->uid, $relationship->rid) : '&nbsp;',
     );
+    if (variable_get('user_relationships_show_user_pictures', 0)) {
+      array_unshift($row, theme('user_picture', $this_user)); 
+    }
+    $rows[] = $row;
   }
 
   print
