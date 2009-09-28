22c12072d379b117f7a214653d54d033d60b0b5d
aufumy <aufumy>
2009-09-28 15:08:59 +0000

    #471546 by aufumy: tt() for autocomplete in user_relationship_views and other translations

--- user_relationship_blocks/templates/user_relationships-pending_block.tpl.php
+++ user_relationship_blocks/templates/user_relationships-pending_block.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: user_relationships-pending_block.tpl.php,v 1.1.2.9 2009-09-23 15:22:48 aufumy Exp $
+// $Id: user_relationships-pending_block.tpl.php,v 1.1.2.10 2009-09-28 15:08:59 aufumy Exp $
 /**
  * @file
  * Template for relationships requests block
@@ -7,9 +7,9 @@
  */
 if ($relationships) {
   $list = array();
-  foreach ($relationships as $relationship) {
-    $tt_rel_name = tt("user_relationships:rtid:$relationship->rtid:name", $relationship->name);
-    $tt_rel_plural_name = tt("user_relationships:rtid:$relationship->rtid:plural_name", $relationship->plural_name);
+  foreach ($relationships as $rtid => $relationship) {
+    $tt_rel_name = tt("user_relationships:rtid:$rtid:name", $relationship->name);
+    $tt_rel_plural_name = tt("user_relationships:rtid:$rtid:plural_name", $relationship->plural_name);
     if ($user->uid == $relationship->requester_id) {
       $relation_to =& $relationship->requestee;
       $controls = theme('user_relationships_pending_request_cancel_link', $user->uid, $relationship->rid);
